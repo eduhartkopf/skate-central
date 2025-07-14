143d437207d998878495e3a58b774ae6693c372f
Jorge Hartkopf <eduhartkopf@gmail.com>
2025-07-14 01:42:59 -0300

    carousel de productos agregado y funcionando correctamente

---
+++ swiper/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: { 
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    720: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  }
+});
